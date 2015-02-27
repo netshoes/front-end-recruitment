12a32e479adfa9b258bdaa08f31bff49d426054d
Hugo Bessa <hugobessaa@gmail.com>
2015-02-27 03:43:42 -0300

    Add Button styles
    For the buy button

--- scripts/components/Bag.js
+++ scripts/components/Bag.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import React from 'react';
+import classnames from 'classnames';
 
 import BagHeader from '../components/BagHeader';
 import BagFooter from '../components/BagFooter';
@@ -15,11 +16,28 @@ let Bag = React.createClass({
     subtotal: PropTypes.object
   },
 
+  onBuyClick(e) {
+    e.preventDefault();
+  },
+
   render() {
+    let buyButtonClasses = classnames({
+      'Button': true,
+      'Button--dark': true,
+      'Button--fill': true,
+      'Button--disabled': this.props.quantity === 0
+    });
+
     return (
       <div className="Bag">
         <BagHeader quantity={this.props.quantity} />
         <BagFooter subtotal={this.props.subtotal} />
+        <a
+          href="#buy"
+          className={buyButtonClasses}
+          onClick={this.onBuyClick}>
+          Comprar
+        </a>
         {console.log(this.props)}
       </div>
     );
