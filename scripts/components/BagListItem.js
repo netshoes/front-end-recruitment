0f776573960c2f38bc7e1e35efc7eb97f78bcbb0
Hugo Bessa <hugobessaa@gmail.com>
2015-02-27 14:04:05 -0300

    Add hoveringRemove styles

--- scripts/components/BagListItem.js
+++ scripts/components/BagListItem.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import React from 'react';
+import classnames from 'classnames';
 
 import ProductThumb from '../components/ProductThumb';
 import ProductPrice from '../components/ProductPrice';
@@ -15,16 +16,40 @@ let BagListItem = React.createClass({
     bagItem: PropTypes.object.isRequired,
   },
 
+  getInitialState() {
+    return {
+      'hoveringRemove': false
+    }
+  },
+
   onRemoveItemClick() {
     let bagActions = flux.getActions('bagItems');
 
     bagActions.removeItem(this.props.bagItem.sku);
   },
 
+  onRemoveItemMouseEnter() {
+    this.setState({
+      'hoveringRemove': true
+    });
+  },
+
+  onRemoveItemMouseLeave() {
+    this.setState({
+      'hoveringRemove': false
+    });
+  },
+
   render() {
     let bagItem = this.props.bagItem;
+
+    let bagListItemClasses = classnames({
+      'BagList-item': true,
+      'BagList-item--hoveringRemove': this.state.hoveringRemove
+    });
+
     return (
-      <div className="BagList-item">
+      <div className={bagListItemClasses}>
         <div className="BagList-thumb">
           <ProductThumb
             sku={bagItem.sku}
@@ -47,7 +72,9 @@ let BagListItem = React.createClass({
         <div className="BagList-removeAndPrice">
           <button
             className="BagList-remove"
-            onClick={this.onRemoveItemClick} />
+            onClick={this.onRemoveItemClick}
+            onMouseEnter={this.onRemoveItemMouseEnter}
+            onMouseLeave={this.onRemoveItemMouseLeave} />
 
           <div className="BagList-price">
             <ProductPrice
