a31f3bd047db1426becf44d3a29d575f1a11f870
Hugo Bessa <hugobessaa@gmail.com>
2015-02-27 15:23:15 -0300

    Fix deprecation warning
    We should explicitly define extended option

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
 let router = express.Router();
