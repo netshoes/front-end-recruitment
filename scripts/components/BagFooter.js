c74958b2bef83dd64e83aa3f7e9c60fda4f8c2e5
Hugo Bessa <hugobessaa@gmail.com>
2015-02-27 14:45:52 -0300

    Add logic to hide subtotal when no items

--- scripts/components/BagFooter.js
+++ scripts/components/BagFooter.js
@@ -15,19 +15,25 @@ let BagFooter = React.createClass({
   render() {
     var { subtotal } = this.props;
 
-    return (
-      <div className="Bag-footer">
-        <span className="Bag-footer-label">Subtotal</span>
-        <span className="Bag-footer-price">
-          <ProductPrice
-              price={subtotal.price}
-              currency={subtotal.currencyFormat}
-              installments={subtotal.installments}
-              smallInstallments={true}
-              gold={true} />
-        </span>
-      </div>
-    );
+    if (subtotal.price > 0) {
+      return (
+        <div className="Bag-footer">
+          <span className="Bag-footer-label">Subtotal</span>
+          <span className="Bag-footer-price">
+            <ProductPrice
+                price={subtotal.price}
+                currency={subtotal.currencyFormat}
+                installments={subtotal.installments}
+                smallInstallments={true}
+                gold={true} />
+          </span>
+        </div>
+      );
+    } else {
+      return (
+        <div className="Bag-footer"></div>
+      );
+    }
   }
 
 });
