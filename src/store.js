a1bab24a5244ed4eb1a1f90932b2ab107d18ccdc
agnezi <jonas.agnezi@gmail.com>
2019-02-20 17:11:56 -0300

    0.1

--- src/store.js
+++ src/store.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { products } from '@/public/data/products.json'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    products
+  },
+  getters: {
   },
   mutations: {
 
